64a4bcb8c71fe06a76a39e71efc4dcdc25df92c1
IoanaTC <ioanateodoraconstantin@yahoo.com>
2023-05-15 00:53:39 +0300

    dictionar

--- proiect/main.py
+++ proiect/main.py
@@ -1,7 +1,7 @@
+import random
 import sys
 sys.path.insert(0, 'src')
 
-import random
 import codificare, selectie, incrucisare, mutatie
 
 def generateInitialSample(population, a, b):
@@ -58,25 +58,24 @@ def getSelectionIntervals(fitnessValues):
 def main(population = 20, a = -1, b = 2, coef_a=-1, coef_b=1, coef_c=2, precision=6, crossover=0.25, mutation=0.01, epochs=50):
     # Generarea populatiei initiale: unde populatie = nr de indivizi, cuprinsi in intervalul [domeniu_a, domeniu_b]
     initialSample = generateInitialSample(population, a, b)
-    newGeneration = initialSample
+    # Obtinerea valorilor de fitness pentru fiecare individ
+    fitnessValues = getFitnessValues(coef_a, coef_b, coef_c, initialSample)
+
+    newGeneration = {i: (initialSample[i], fitnessValues[i]) for i in range(population)}
 
     # Timp de un nr de generatii dat de epochs, vom simula evolutia indivizilor generati in initialSample
     for _ in range(1):
         oldGeneration = newGeneration
+        # Crearea unei nnoi generatii pornind de la cea precedenta
         newGeneration = []
 
-        # Codificarea indivizilor idn populatia curenta
-        binaryString_length, sizeOfBin = codificare.codeConditions(a, b, precision)
-        binaryRepresentation = getBinaryList(initialSample, a, binaryString_length, sizeOfBin)
+        # # Codificarea indivizilor idn populatia curenta
+        # binaryString_length, sizeOfBin = codificare.codeConditions(a, b, precision)
+        # binaryRepresentation = getBinaryList(initialSample, a, binaryString_length, sizeOfBin)
 
-        # Obtinerea valorilor de fitness pentru fiecare individ
-        fitnessValues = getFitnessValues(coef_a, coef_b, coef_c, oldGeneration)
-
-        # criteriul elitist, individul cu cea mai mare valoare a functiei de fit trece in generatia urmatoare
-        eliteIndex = fitnessValues.index(max(fitnessValues))
-        newGeneration.append(eliteIndex)
-        oldGeneration.pop(eliteIndex)
-        fitnessValues.pop(eliteIndex)
+        # criteriul elitist, gasirea individului cu cea mai mare valoare a functiei de fit
+        eliteIndividual = max(oldGeneration.items(), key=lambda x : x[1][1])
+        newGeneration = {eliteIndividual[0] : eliteIndividual}
 
         # Stabilirea intervalelor de selectie
         fitnessIntervals = getSelectionIntervals(fitnessValues)
@@ -97,7 +96,32 @@ def main(population = 20, a = -1, b = 2, coef_a=-1, coef_b=1, coef_c=2, precisio
                 else:
                     right = mij - 1
             
-            newGeneration.append(left)
-        print(newGeneration)
+            newGeneration[left] = oldGeneration[left]
 
+        # Generarea unor noi probabilitati pentru a testa daca un individ este apt pentru any crossover sau mutation
+        individualProbability = generateIndividualProbability(population - 1)
+
+        x1, x2 = None, None
+        for index, p in enumerate(individualProbability):
+            probability = float(p)
+
+            if x1 is not None and x2 is not None:
+                punct_rupere = random.randint(0, binaryString_length)
+
+                newGeneration.remove(x1)
+                newGeneration.remove(x2)
+                cromozom_nou1, cromozom_nou2 = incrucisare.crossover(x1, x2, int(punct_rupere))
+                newGeneration.append(cromozom_nou1, cromozom_nou2)
+                
+                x1 = None
+                x2 = None
+                continue
+
+            if probability < crossover:
+                if x1 is None:
+                    x1 = newGeneration[index]
+                else:
+                    x2 = newGeneration[index]
+
+        print(newGeneration)
 main()
